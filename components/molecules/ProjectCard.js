d5391485f907c44065480af730ffa9b9dd035a12
Mateo Buitrago Jara <mateobuitragojara@192.168.0.104>
2019-08-10 13:05:39 +0200

    trying image loader

--- components/molecules/ProjectCard.js
+++ components/molecules/ProjectCard.js
@@ -14,9 +14,32 @@ const StyledProjectCard = styled.div`
     }
 `
 
+const Placeholder = styled.div`
+    height: 250px;
+    width: auto;
+    background: red;
+`
+
 class ProjectCard extends React.Component{
     constructor(){
         super();
+        this.state = { isLoading: true }
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: false})
+    }
+
+    renderImage(image){
+        let imageElement;
+
+        if(this.state.isLoading){
+            imageElement = <Placeholder/>
+        } else{
+            imageElement = <img src={image}></img>
+        }
+
+        return imageElement;
     }
 
     getRandomMargin(){
@@ -34,7 +57,7 @@ class ProjectCard extends React.Component{
         return(
             <Link href="/work/[id]" as={`/work/${this.props.id}`}>            
                 <StyledProjectCard>
-                    <img src={this.props.img}></img>
+                    {this.renderImage(this.props.img)}
                     <Typography mb="0" type="body" dark bold>{this.props.name}</Typography>
                     <Typography type="body" dark>{this.props.tagline}</Typography>
                     <Tag>{this.props.tag}</Tag>
