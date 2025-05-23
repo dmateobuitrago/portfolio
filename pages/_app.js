ff378cd1d660a7f7a1a8910d0f3ced2c8c317153
dmateobuitrago <dmateobuitrago@gmail.com>
2025-05-23 10:00:28 +0200

    added rebranded project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import App, { Container } from "next/app";
-import React from "react";
+import App from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { tema } from "../styles/tema";
 
@@ -7,9 +7,18 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
+      <>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0"
+          />
+          <title>Mateo Buitrago Jara - Senior Product Designer</title>
+        </Head>
         <ThemeProvider theme={tema}>
           <Component {...pageProps} />
         </ThemeProvider>
+      </>
     );
   }
 }
