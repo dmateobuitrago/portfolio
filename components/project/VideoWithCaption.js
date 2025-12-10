1acd37127d5e11fd53824c31b4fe275f76f695da
dmateobuitrago <dmateobuitrago@gmail.com>
2025-12-10 19:25:16 -0300

    fixed video component

--- components/project/VideoWithCaption.js
+++ components/project/VideoWithCaption.js
@@ -6,40 +6,52 @@ import styled from "styled-components";
 
 const imgPath = "/static/images/";
 
-const MyVideo = styled.video` 
+const MyVideo = styled.video`
   max-width: 100%;
-`
+  max-height: 520px;
+`;
 
 const Arrow = styled(Typography)`
-  ::before {
+  &::before {
     content: "↑";
   }
-  @media screen and (min-width: ${props => props.theme.minBreakPoints.medium}) {
+  @media screen and (min-width: ${(props) =>
+      props.theme.minBreakPoints.medium}) {
     &::before {
       content: "←";
     }
   }
 `;
 
-
-const GridBlockJustifyEnd = styled(GridBlock)`
+const CaptionContainer = styled(GridBlock)`
   flex-direction: column;
   justify-content: end;
+  padding-top: ${(props) => props.theme.baseUnit2};
+  @media screen and (min-width: ${(props) =>
+      props.theme.minBreakPoints.medium}) {
+    padding-inline: ${(props) => props.theme.baseUnit2};
+  }
 `;
 
 class VideoWithCaption extends React.Component {
+  renderArrow() {
+    if (this.props.body) {
+      return <Arrow type="body" dark mb="0" />;
+    }
+  }
+
   render() {
     return (
-      <OneBigColumn reverse>
-        <GridBlock col="8" colMedium="6" padding>
+      <OneBigColumn>
+        <GridBlock col="8" colMedium="6">
           <MyVideo src={imgPath + this.props.video} controls></MyVideo>
         </GridBlock>
-        <GridBlockJustifyEnd col="8" colMedium="2" padding isGridContainer>
-          <Arrow type="body" dark mb="0" />
+        <CaptionContainer col="8" colMedium="2" isGridContainer>
+          {this.renderArrow()}
           <Typography type="small" dark mb="0">
             {this.props.body}
           </Typography>
-        </GridBlockJustifyEnd>
+        </CaptionContainer>
       </OneBigColumn>
     );
   }
