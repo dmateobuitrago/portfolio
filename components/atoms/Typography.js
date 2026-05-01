6c093881fe6b604c408f1f2fb5106f0f16e7b153
dmateobuitrago <dmateobuitrago@gmail.com>
2026-05-01 14:38:18 +0200

    feat: migrate Typography to CSS modules

--- components/atoms/Typography.js
+++ components/atoms/Typography.js
@@ -1,86 +1,26 @@
 import React from "react";
-import styled from "styled-components";
-
-const Base = styled.div`
-  color: ${(props) =>
-    props.$dark ? props.theme.primary : props.theme.secondary};
-  font-family: ${(props) => props.theme.font.fontFamily};
-  font-weight: ${(props) => (props.$bold ? "700" : "300")};
-  text-align: ${(props) => (props.$align ? props.$align : "left")};
-`;
-
-const Huge = styled(Base)`
-  font-size: ${(props) => props.theme.font.size.xLarge};
-  line-height: 1.1;
-  @media screen and (min-width: ${props => props.theme.minBreakPoints.large}){
-    font-size: ${(props) => props.theme.font.size.xxLarge};
-  }
-`;
-
-const Title = styled(Base)`
-  font-size: ${(props) => props.theme.font.size.large};
-  line-height: 1.5;
-  @media screen and (min-width: ${props => props.theme.minBreakPoints.large}){
-    font-size: ${(props) => props.theme.font.size.xLarge};
-  }
-`;
-
-const Subtitle = styled(Base)`
-  font-size: ${(props) => props.theme.font.size.medium};
-  line-height: 1.5;
-  @media screen and (min-width: ${props => props.theme.minBreakPoints.large}){
-    font-size: ${(props) => props.theme.font.size.large};
-  }
-`;
-
-const Body = styled(Base)`
-  font-size: ${(props) => props.theme.font.size.medium};
-  line-height: 1.5;
-`;
-
-const Small = styled(Base)`
-  font-size: ${(props) => props.theme.font.size.small};
-  line-height: 1.5;
-`;
+import styles from "./Typography.module.css";
 
 class Typography extends React.Component {
-  formatContent(content) {
-    let formatedContent;
-    if (typeof content === "string" || content instanceof String) {
-      formatedContent = content.replace("*(", "&lt;strong&gt;");
-      formatedContent = formatedContent.replace(")*", "&lt;/strong&gt;");
-    }
-
-    return formatedContent;
-  }
-
   render() {
-    let content = this.props.children;
+    const { type, bold, dark, align, className, children } = this.props;
 
-    const { type, bold, dark, align, mb, className } = this.props;
-
-    const sharedProps = {
+    const classes = [
+      styles.base,
+      styles[type] || styles.body,
+      dark ? styles.dark : styles.light,
+      bold ? styles.bold : null,
       className,
-      $bold: bold,
-      $align: align,
-      $dark: dark,
-      $mb: mb,
-    };
+    ].filter(Boolean).join(" ");
 
-    switch (type) {
-      case "title":
-        return <Title {...sharedProps}>{content}</Title>;
-      case "subtitle":
-        return <Subtitle {...sharedProps}>{content}</Subtitle>;
-      case "huge":
-        return <Huge {...sharedProps}>{content}</Huge>;
-      case "body":
-        return <Body {...sharedProps}>{content}</Body>;
-      case "small":
-        return <Small {...sharedProps}>{content}</Small>;
-      default:
-        return <Body {...sharedProps}>{content}</Body>;
-    }
+    return (
+      <div
+        className={classes}
+        style={align ? { textAlign: align } : undefined}
+      >
+        {children}
+      </div>
+    );
   }
 }
 
